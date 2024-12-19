cf8e14caff68b76f4c52d269351e7b621faf1d3c
Danil-Pq <fertyujh.8op7@gmail.com>
2024-12-19 20:08:38 +0300

    Update TASK 6: Многомерное динамические массивы.cpp
    3 и 4 не смог сделать

--- TASK 6: Многомерное динамические массивы.cpp
+++ TASK 6: Многомерное динамические массивы.cpp
@@ -1,3 +1,4 @@
+Задание №1
 #include <iostream>
 #include <vector>
 
@@ -63,3 +64,61 @@ int main() {
 Матрица после добавления столбца:
 1 2 5 
 3 4 6 
+
+
+
+Задание №2
+
+#include <iostream>
+#include <vector>
+
+void removeColumn(std::vector<std::vector<int>>& matrix, int position) {
+    for (auto& row : matrix) {
+        row.erase(row.begin() + position);
+    }
+}
+
+int main() {
+
+    int m, n;
+    std::cout << "Введите количество строк: ";
+    std::cin >> m;
+    std::cout << "Введите количество столбцов: ";
+    std::cin >> n;
+
+    std::vector<std::vector<int>> matrix(m, std::vector<int>(n));
+    std::cout << "Введите элементы матрицы: \n";
+    for (auto& row : matrix) {
+        for (auto& val : row) {
+            std::cin >> val;
+        }
+    }
+
+    int position;
+    std::cout << "Введите позицию для удаления столбца: ";
+    std::cin >> position;
+
+    removeColumn(matrix, position);
+
+    std::cout << "Матрица после удаления столбца: \n";
+    for (const auto& row : matrix) {
+        for (int val : row) {
+            std::cout << val << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    return 0;
+}
+
+
+Ввод:
+Введите количество строк: 2
+Введите количество столбцов: 3
+Введите элементы матрицы: 
+1 2 3 4 5 6 
+Введите позицию для удаления столбца: 2
+Вывод:
+Матрица после удаления столбца: 
+1 2 
+4 5 
